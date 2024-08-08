b5114052cf7fb14d75c3157bb63aaab7dd592f36
ChethanKodase <chethankr27@gmail.com>
2024-08-08 12:17:01 +0200

    updates

---
+++ 3D_unet.py
@@ -0,0 +1,167 @@
+import tensorflow as tf
+import keras
+print(tf.__version__)
+print(keras.__version__)
+
+
+#Make sure the GPU is available. 
+import tensorflow as tf
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
+
+'''
+export CUDA_VISIBLE_DEVICES=3
+
+cd stelar_3d/
+conda activate stlr3
+python 3D_unet.py 
+
+
+'''
+
+
+
+import segmentation_models_3D as sm
+
+
+from skimage import io
+from patchify import patchify, unpatchify
+import numpy as np
+from matplotlib import pyplot as plt
+from keras import backend as K
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+
+num_epochs = 1500
+vista_crop_dict = { 0:'NA' , 1: 'ALFALFA', 2: 'BEET', 3: 'CLOVER', 4: 'FLAX', 5: 'FLOWERING_LEGUMES', 6: 'FLOWERS', 7: 'FOREST', 8: 'GRAIN_MAIZE', 9: 'GRASSLAND', 10: 'HOPS', 11: 'LEGUMES', 12: 'NA', 13: 'PERMANENT_PLANTATIONS', 14: 'PLASTIC', 15: 'POTATO', 16: 'PUMPKIN', 17: 'RICE', 18: 'SILAGE_MAIZE', 19: 'SOY', 20: 'SPRING_BARLEY', 21: 'SPRING_OAT', 22: 'SPRING_OTHER_CEREALS', 23: 'SPRING_RAPESEED', 24: 'SPRING_RYE', 25: 'SPRING_SORGHUM', 26: 'SPRING_SPELT', 27: 'SPRING_TRITICALE', 28: 'SPRING_WHEAT', 29: 'SUGARBEET', 30: 'SUNFLOWER', 31: 'SWEET_POTATOES', 32: 'TEMPORARY_GRASSLAND', 33: 'WINTER_BARLEY', 34: 'WINTER_OAT', 35: 'WINTER_OTHER_CEREALS', 36: 'WINTER_RAPESEED', 37: 'WINTER_RYE', 38: 'WINTER_SORGHUM', 39: 'WINTER_SPELT', 40: 'WINTER_TRITICALE', 41: 'WINTER_WHEAT'}
+
+
+all_input_img = []
+all_input_mask = []
+for a in range(40):
+    input_img = io.imread('/home/luser/stelar_3d/storage/data_labels/train'+str(a)+'.tif')
+    input_mask = io.imread('/home/luser/stelar_3d/storage/data_labels/lab'+str(a)+'.tif').astype(np.uint8)
+    all_input_img.append(input_img)
+    all_input_mask.append(input_mask)
+
+input_img = np.array(all_input_img).reshape(-1, 64, 64, 64)
+input_mask = np.array(all_input_mask).reshape(-1, 64, 64)
+input_mask = np.repeat(input_mask[:, np.newaxis, :, :], repeats=64, axis=1)
+
+
+unique_elements, element_counts = np.unique(input_mask, return_counts=True)
+
+
+all_input_img = 0
+all_input_mask = 0
+
+
+input_mask[input_mask==0]=0
+input_mask[input_mask==1]=1
+input_mask[input_mask==7]=2
+input_mask[input_mask==8]=3
+input_mask[input_mask==9]=0
+input_mask[input_mask==11]=0
+input_mask[input_mask==13]=0
+input_mask[input_mask==32]=0
+#input_mask = np.repeat(input_mask[:, np.newaxis, :, :], repeats=64, axis=1)
+
+#input_img[input_img==0] = np.median(input_img)
+
+'''for m in range(64):
+    for i in range(64):
+        for j in range(64):
+            input_img[m,:,i,j][input_img[m,:,i,j]==0] = np.median(input_img[m,:,i,j]).astype(np.uint8)'''
+
+lai_uniques = np.unique(input_img)
+
+for n in range(len(lai_uniques)):
+  input_img[input_img==lai_uniques[n]]=n
+input_img = input_img.astype(np.uint8)
+
+
+lai_uniques = 0
+n_classes=4
+
+
+
+train_img = np.stack((input_img,)*3, axis=-1)
+train_mask = np.expand_dims(input_mask, axis=4)
+train_mask_cat = to_categorical(train_mask, num_classes=n_classes)
+X_train, X_test, y_train, y_test = train_test_split(train_img, train_mask_cat, test_size = 0.10, random_state = 0)
+
+
+train_img = 0
+train_mask_cat = 0
+train_mask = 0
+input_mask = 0
+input_img = 0
+
+
+
+def dice_coefficient(y_true, y_pred):
+    smoothing_factor = 1
+    flat_y_true = K.flatten(y_true)
+    flat_y_pred = K.flatten(y_pred)
+    return (2. * K.sum(flat_y_true * flat_y_pred) + smoothing_factor) / (K.sum(flat_y_true) + K.sum(flat_y_pred) + smoothing_factor)
+
+def dice_coefficient_loss(y_true, y_pred):
+    return 1 - dice_coefficient(y_true, y_pred)
+
+
+
+
+encoder_weights = 'imagenet'
+BACKBONE = 'vgg16'  #Try vgg16, efficientnetb7, inceptionv3, resnet50
+activation = 'softmax'
+patch_size = 64
+n_classes = 4
+channels=3
+
+LR = 0.0001
+optim = keras.optimizers.Adam(LR)
+
+dice_loss = sm.losses.DiceLoss(class_weights=np.array([0.25, 0.25, 0.25, 0.25])) 
+focal_loss = sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+
+
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+X_train_prep = preprocess_input(X_train)
+X_test_prep = preprocess_input(X_test)
+
+
+X_train = 0
+X_test = 0
+
+
+#Define the model. Here we use Unet but we can also use other model architectures from the library.
+model = sm.Unet(BACKBONE, classes=n_classes, 
+                input_shape=(patch_size, patch_size, patch_size, channels), 
+                encoder_weights=encoder_weights,
+                activation=activation)
+
+model.compile(optimizer = optim, loss=total_loss, metrics=metrics)
+print(model.summary())
+
+
+
+#Fit the model
+history=model.fit(X_train_prep, 
+          y_train,
+          batch_size=8, 
+          epochs=num_epochs,
+          verbose=1,
+          validation_data=(X_test_prep, y_test))
+
+
+
+model.save('/home/luser/stelar_3d/storage/saved_model/3D_unet_labels_'+vista_crop_dict[0]+'_'+vista_crop_dict[1]+'_'+vista_crop_dict[7]+'_'+vista_crop_dict[8]+'_num_epocs_'+str(num_epochs)+'.h5')
